e722b3d4ac5bc06228ebdd8bd37ce704d6f85b45
Фролкин Иван Михайлович <imfrolki@mts.ru>
2025-06-14 03:13:06 +0300

    further

--- app/receipt/components/FormArrayTitle.tsx
+++ app/receipt/components/FormArrayTitle.tsx
@@ -5,20 +5,18 @@ interface FormTitleProps {
   title: string;
   onAddClick?: () => void;
   className?: string;
-  showColon?: boolean;
 }
 
 export const FormArrayTitle: React.FC<FormTitleProps> = ({
   title,
   onAddClick,
   className = "",
-  showColon = false
 }) => {
   return (
     <div className={`flex items-center ${className}`}>
       <AddButton onClick={onAddClick} className="mr-2" />
       <span className="flex-grow">
-        {title}{showColon ? ":" : ""}
+        {title}
       </span>
     </div>
   );
