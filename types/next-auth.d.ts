5dae479c8a2ead342a2efb76546edd09a7f296b4
Ivan Frolkin <redsool@gmail.com>
2025-12-14 23:10:55 +0300

    fix

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,6 @@
 import { DefaultSession } from "next-auth";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JWT } from "next-auth/jwt";
 declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
