6d65fbdf9e4b28a771ee34e737f3f97b1587dba1
Ivan Frolkin <redsool@gmail.com>
2025-12-19 04:31:59 +0300

    webpack

--- next.config.js
+++ next.config.js
@@ -4,9 +4,22 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // @ts-check
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withBundleAnalyzer({
   output: "standalone",
   serverExternalPackages: ["@prisma/adapter-neon"],
-};
+  webpack(config, { isServer, dev }) {
+    // Use the client static directory in the server bundle and prod mode
+    // Fixes `Error occurred prerendering page "/"`
+    config.output.webassemblyModuleFilename =
+      isServer && !dev
+        ? "../static/wasm/[modulehash].wasm"
+        : "static/wasm/[modulehash].wasm";
+
+    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+
+    return config;
+  },
+});
 
 module.exports = nextConfig;
