a7f8b97802ce9ca2cd8072a8a0b33276fa3b66b8
Ivan Frolkin <redsool@gmail.com>
2026-01-07 08:38:30 +0300

    fix

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { createContext, useContext } from "react";
-import { User } from "@supabase/supabase-js";
+import { createContext, useContext, useEffect, useState } from "react";
+import { User, Session, AuthChangeEvent } from "@supabase/supabase-js";
+import { supabase } from "@/utils/supabase/client";
 
 interface AuthContextType {
     user: User;
@@ -16,9 +17,29 @@ export const AuthProvider = ({
     children: React.ReactNode;
     initialUser: User
 }) => {
+    const [user, setUser] = useState(initialUser);
+
+    useEffect(() => {
+
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange(
+            (_: AuthChangeEvent, session: Session | null) => {
+                if (session?.user) {
+                  setUser(session.user);
+                } else {
+                  window.location.reload();
+                }
+            }
+        );
+
+        return () => {
+            subscription.unsubscribe();
+        };
+    }, []);
 
     return (
-        <AuthContext.Provider value={{ user: initialUser }}>
+        <AuthContext.Provider value={{ user }}>
             {children}
         </AuthContext.Provider>
     );
